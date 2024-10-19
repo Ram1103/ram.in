ac1f4e0564cec185455bcc8acf27e9f777b406f3
Ram <a.ramachandran.2002@gmail.com>
2024-10-19 23:37:39 +0530

    Changing Landing page

---
+++ src/data/index.ts
@@ -0,0 +1,27 @@
+export const SITE_TITLE = "Ram's Space";
+export const SITE_DESCRIPTION = "Internet space of a uncommon generalist.";
+
+export interface MenuItem {
+	label: string;
+	url: string;
+}
+
+// Menu items
+export const menuItems: MenuItem[] = [
+	{
+		label: "home",
+		url: "/",
+	},
+	{
+		label: "writings",
+		url: "/writings",
+	},
+	{
+		label: "thoughts",
+		url: "/thoughts",
+	},
+	{
+		label: "ships",
+		url: "/ships",
+	},
+];
