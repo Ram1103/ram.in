622a591011156db4f4b489035247e33e1093ccfc
Ram <a.ramachandran.2002@gmail.com>
2024-10-20 19:53:56 +0530

    updating landing page

---
+++ src/content/whoami/multiliner.ts
@@ -0,0 +1,10 @@
+export const textColorPairs=[
+    { text: "generalist.", colorClass: "text-red-400" },
+    { text: "designer.", colorClass: "text-green-400" },
+    { text: "product geek.", colorClass: "text-blue-400" },
+    { text: "software developer.", colorClass: "text-yellow-400" },
+    { text: "ai dreamer.", colorClass: "text-orange-400" },
+    { text: "extrovert.", colorClass: "text-purple-400" },
+    { text: "community vibe keeper.", colorClass: "text-indigo-400" },
+    { text: "coffee addict.", colorClass: "text-pink-400" },
+  ];
